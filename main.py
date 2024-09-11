39fc9e90d2fb8e32fce912d5a0dc67769eecd864
grass2440 <38907905+grass2440@users.noreply.github.com>
2024-09-12 01:06:40 +0800

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
+This code is modified based on the code from the following papers.
+
+@InProceedings{Zheng_2022_CVPR,
+    author    = {Zheng, Tu and Huang, Yifei and Liu, Yang and Tang, Wenjian and Yang, Zheng and Cai, Deng and He, Xiaofei},
+    title     = {CLRNet: Cross Layer Refinement Network for Lane Detection},
+    booktitle = {Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)},
+    month     = {June},
+    year      = {2022},
+    pages     = {898-907}
+}
+    
+@article{huang2020ccnet,
+  author={Huang, Zilong and Wang, Xinggang and Wei, Yunchao and Huang, Lichao and Shi, Humphrey and Liu, Wenyu and Huang, Thomas S.},
+  journal={IEEE Transactions on Pattern Analysis and Machine Intelligence}, 
+  title={CCNet: Criss-Cross Attention for Semantic Segmentation}, 
+  year={2020},
+  month={},
+  volume={},
+  number={},
+  pages={1-1},
+  keywords={Semantic Segmentation;Graph Attention;Criss-Cross Network;Context Modeling},
+  doi={10.1109/TPAMI.2020.3007032},
+  ISSN={1939-3539}}
+
+@article{huang2018ccnet,
+    title={CCNet: Criss-Cross Attention for Semantic Segmentation},
+    author={Huang, Zilong and Wang, Xinggang and Huang, Lichao and Huang, Chang and Wei, Yunchao and Liu, Wenyu},
+    booktitle={ICCV},
+    year={2019}}
+
+
+
+
+
 import os
 import cv2
 import torch
